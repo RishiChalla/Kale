39827cbdddcd8bf3e892ba85543e10d83553debc
RishiChalla <rishik.challa@gmail.com>
2021-10-21 13:26:05 -0400

    Changed to Static Library, added Mainpage Docs, & Kale Logo

---
+++ src/Docs/MainPage.cpp
@@ -0,0 +1,39 @@
+/*
+   Copyright 2021 Rishi Challa
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+/**
+ * @mainpage Kale Documentation
+ * > Full disclosure that this is a closed source project, this is only ever intended to be read by the author. If you are
+ * > not the author of this software or do not have their permission please delete the software immediately.
+ * 
+ * @section s1 What is Kale?
+ * Kale is a 2D Game Engine made with Vulkan with specialized "blobby" graphics. Kale supports Mac OSX, Windows, Linux,
+ * ~~IOS~~, ~~Android~~, and the ~~Nintendo Switch~~ (Striked platforms currently do not have support but are planned in
+ * the near future). This guide will teach you how to get back on your feet and use Kale for development if you ever forget.
+ * 
+ * @section s2 Getting Started - Editor
+ * > The editor has not been coded yet however once it has been the following steps will apply.
+ * Open up the Editor application and create a new project, follow the GUI and create a project name, set the project directory,
+ * choose the project platforms, and click create. From there you can follow the editor guides for further information regarding usage.
+ * 
+ * TODO - Finish this section after the editor is created
+ * 
+ * @section s3 Getting Started - Code/CMake
+ * Create a new directory for your project and create a new file called `CMakeLists.txt` and a new directory for your source code.
+ * 
+ * TODO - Expand on this section
+ * 
+ */
