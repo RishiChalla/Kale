17263cea2654051dd2064a3884c725309e81e2d7
RishiChalla <rishik.challa@gmail.com>
2021-10-29 11:32:40 -0400

    Added setup instructions to docs mainpage

--- src/Kale/Docs/MainPage.cpp
+++ src/Kale/Docs/MainPage.cpp
@@ -33,7 +33,100 @@
  * 
  * @section s3 Getting Started - Code/CMake
  * Create a new directory for your project and create a new file called `CMakeLists.txt` and a new directory for your source code.
+ * Go inside your new directory and clone the github repo - 
+ * ```shell
+ * cd YourProjectDirectory
+ * git clone https://github.com/RishiChalla/Kale
+ * cd Kale
+ * git submodule init
+ * git submodule update
+ * cd ../
+ * ```
  * 
- * TODO - Expand on this section
+ * Then in your parent directory paste the following in your `CMakeLists.txt` and change the project name for your project.
+ * ```cmake
+ * cmake_minimum_required(VERSION 3.0.0)
+ * project(KaleTesting VERSION 1.0)
+ * 
+ * set(CMAKE_CXX_STANDARD 17)
+ * set(CMAKE_CXX_STANDARD_REQUIRED True)
+ * 
+ * file(GLOB_RECURSE sources CONFIGURE_DEPENDS src/*.cpp src/*.hpp)
+ * 
+ * file(GLOB_RECURSE sources CONFIGURE_DEPENDS src/*.cpp src/*.hpp)
+ * add_executable(KaleTesting ${sources})
+ * source_group(TREE "${CMAKE_CURRENT_LIST_DIR}" FILES ${sources})
+ * 
+ * target_include_directories(KaleTesting PUBLIC src/)
+ * 
+ * set(CPACK_PROJECT_NAME ${PROJECT_NAME})
+ * set(CPACK_PROJECT_VERSION ${PROJECT_VERSION})
+ * include(CPack)
+ * 
+ * # Kale
+ * add_subdirectory(Kale)
+ * target_link_libraries(KaleTesting Kale)
+ * 
+ * # Set the assets folder
+ * set(assetsOutputFolder $<TARGET_FILE_DIR:${PROJECT_NAME}>/.KaleTesting/assets)
+ * 
+ * # Copy all resources (assets, shaders, etc)
+ * add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
+ *                    COMMAND ${CMAKE_COMMAND} -E copy_directory
+ *                        ${CMAKE_SOURCE_DIR}/assets ${assetsOutputFolder})
+ * 
+ * add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
+ * 					COMMAND ${CMAKE_COMMAND} -E copy_directory
+ * 					   ${CMAKE_SOURCE_DIR}/Kale/assets ${assetsOutputFolder})
+ * ```
+ * 
+ * Add an src folder and create two files: `App.hpp` and `App.cpp` within it as a simple test. Copy the code below into each file to ensure
+ * Kale is properly linked and working.
+ * 
+ * > App.hpp
+ * ```c++
+ * #pragma once
+ * 
+ * #include <Kale/Application/Application.hpp>
+ * 
+ * namespace KaleTesting {
+ * 	
+ * 	// Represents the main application class
+ * 	class App : public Kale::Application {
+ * 
+ * 	protected:
+ * 		
+ * 		// Called when the application begins
+ * 		void onBegin() override;
+ * 	
+ * 	public:
+ * 
+ * 		// Creates a new app instance
+ * 		App();
+ * 	};
+ * }
+ * ```
+ * 
+ * > App.cpp
+ * ```c++
+ * #include "App.hpp"
+ * 
+ * using namespace KaleTesting;
+ * 
+ * // Heap allocates the application
+ * Kale::Application* createApplication() {
+ * 	return new App();
+ * }
+ * 
+ * // Creates a new app instance
+ * App::App() : Kale::Application("KaleTesting") {
+ * 	// Empty constructor - nothing to do here.
+ * }
+ * 
+ * // Called when the application begins
+ * void App::onBegin() {
+ * 	klPrint("Hello world!");
+ * }
+ * ```
  * 
  */
